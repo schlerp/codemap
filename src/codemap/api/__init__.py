77f8478dfc3373664ea4d08f7e2a1f09d5417a93
PattyC <patrickcoffey91@gmail.com>
2022-10-16 13:12:20 +1100

    updated readme

--- src/codemap/api/__init__.py
+++ src/codemap/api/__init__.py
@@ -7,7 +7,6 @@ from codemap.api.v1.router import code_router
 
 description = "An application for managing code sets, and enabling easy access to them via a rest API."
 
-
 app = fastapi.FastAPI(
     title="CodeMap",
     description=description,
