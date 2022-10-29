f25155e27b6b5d8601b2d57deefb374cf7978f41
PattyC <patrickcoffey91@gmail.com>
2022-10-29 22:06:09 +0930

    moved to neo4j

--- src/codemap/api/__init__.py
+++ src/codemap/api/__init__.py
@@ -1,8 +1,5 @@
 import fastapi
 from codemap import config
-from codemap.adapaters import Base
-from codemap.adapaters import database
-from codemap.adapaters import engine
 from codemap.api.v1.router import code_router
 
 description = "An application for managing code sets, and enabling easy access to them via a rest API."
@@ -13,14 +10,3 @@ app = fastapi.FastAPI(
     version=config.VERSION,
 )
 app.include_router(code_router.router)
-
-
-@app.on_event("startup")
-async def startup():
-    Base.metadata.create_all(engine)
-    await database.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
---
+++ tests/test_model_to_schema.py
@@ -0,0 +1,18 @@
+from codemap import models
+from codemap import schemas
+from codemap.persist import model_to_schema
+
+
+def test_code():
+    test_model = models.Code(key="test_key", value="test_value")
+    test_schema = model_to_schema(test_model, schemas.Code)
+    assert isinstance(test_schema, schemas.Code)
+    assert test_schema.key == test_model.key
+    assert test_schema.value == test_model.value
+
+
+def test_codeset_empty():
+    test_model = models.CodeSet(name="test_name")
+    test_schema = model_to_schema(test_model, schemas.CodeSet)
+    assert isinstance(test_schema, schemas.CodeSet)
+    assert test_schema.name == test_model.name
